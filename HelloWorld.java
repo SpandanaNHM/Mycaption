c8421b15f080f695d9b7b882d51ca52193e527dc
SpandanaNHM <75571762+SpandanaNHM@users.noreply.github.com>
2020-12-06 19:30:51 +0530

    Created HelloWorld.java
    This prints the HELLO WORLD on screen.

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+package samplehello;
+
+/**
+ *
+ * @author HP
+ */
+public class Samplehello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("HELLO WORLD");
+    }
+    
+}
+
